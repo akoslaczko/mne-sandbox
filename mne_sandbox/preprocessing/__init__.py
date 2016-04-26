c5a62f06b45108bbd68b32a2b297ac6ced150b0c
Marijn van Vliet <w.m.vanvliet@gmail.com>
2016-04-26 10:39:01 +0300

    Implementation of three of the FASTER steps.
    Adds a framework for marking bad channels/epochs based on outlier
    detection. For now, three of the steps of the FASTER pipeline are
    implemented:
    
     1. Detect bad channels
     2. Detect bad epochs
     3. Detect bad channels in epochs
    
    See:
    H., Whelan R. and Reilly RB. Engemann D. FASTER: fully
    automated statistical thresholding for EEG artifact rejection.
    Journal of Neuroscience Methods, vol. 192, issue 1, pp. 152-162, 2010.

--- mne_sandbox/preprocessing/__init__.py
+++ mne_sandbox/preprocessing/__init__.py
@@ -1,2 +1,5 @@
 from .eog import eog_regression
 from ._dss import dss
+from .bads import (find_outliers,
+                   find_bad_channels, find_bad_epochs,
+                   find_bad_channels_in_epochs)
